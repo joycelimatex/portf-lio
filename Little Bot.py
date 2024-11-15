855a7f09ac57b480a19aecabb5bd94f1b0abf00d
joycelimatex <joycelimamkt@gmail.com>
2024-11-15 17:39:13 -0300

    Add files via upload

---
+++ Little Bot.py
@@ -0,0 +1,9 @@
+name = input ("Hello, what is your name?")
+print (f"nice to meet you, {name}!")
+age_input = input ("How old are you?")
+age = int(age_input)
+bot_age = 3
+age_difference = age - bot_age 
+print (f"You are {age_difference} years old than me. I'm only {bot_age} years old!")
+color = input ("What is your favorite color?")
+print (f"Oh, {color} is a beautiful color.")
